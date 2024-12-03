34cbf3db08e9a8519a75006eada8d5d8f23dcd0d
Lorraine Scanlon <lorrainescanlon@gmail.com>
2024-12-03 23:52:37 +0000

    added .py file

---
+++ sql-psycopg2.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+
+# connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1 - select all records from "Artist" table
+cursor.execute('SELECT * FROM "Artist"')
+
+# fecth the results (multiple)
+results = cursor.fetchall()
+
+# fectch the result (single)
+#results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print results
+for result in results:
+    print(result)
+
+
