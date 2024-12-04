863e94ea0f01e71fab8af2a9375c040ac854bd8e
Lorraine Scanlon <lorrainescanlon@gmail.com>
2024-12-04 22:54:15 +0000

    setup classes in sql-orm.py

---
+++ sql-orm.py
@@ -0,0 +1,54 @@
+from sqlalchemy import (
+    create_engine, Column, Fliat, ForeignKey, Integer, String
+)
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+#executing the instructions from the "chinook" database
+db = create_engine("postgresql ///chinook")
+base = declarative_base()
+
+# create a class-based model for the "Artist" table
+class Artist(base):
+    __tablename__ = "Artist"
+    ArtistId = Column(Integer, primary_key=True)
+    Name = Column(String)
+
+
+# create a class-based model for the "Album" table
+class Album(base):
+    __tablename__ = "Album"
+    AlbumId = Column(Integer, primary_key=True)
+    Title = Column(String)
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
+
+
+# create a class-based model for the "Track" table
+class Track(base):
+    __tablename__ = "Track"
+    TrackId = Column(Integer, primary_key=True)
+    Name =vColumn(String)
+    AlbumId = Column(Integer, ForeignKey("Album.AlbmId"))
+    MediaTypeId = Column(Integer, primary_key=False)
+    GenreId = Column(Integer, primary_key=False)
+    Composer = Column(String)
+    Milliseconds = Column(Integer, primary_key=False)
+    Bytes = Column(Integer, primary_key=False)
+    UnitPrice = Column(float)
+
+
+
+# instead of connecting to the database directly, we will ask for a sessio
+# create a new instance of sessionmaker, then point to our engine (the db)
+Session = sessionmaker(db)
+# opens an actual session by calling the Session() subclass defined above
+session = Session()
+
+# creating the database declarative_base subclass
+base.metadata.create_all(db)
+
+
+
+
