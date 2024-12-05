6f4fa83b97008da891a98a32830748417dbc3577
Lorraine Scanlon <lorrainescanlon@gmail.com>
2024-12-05 12:33:15 +0000

    update and delete records

--- sql-crud.py
+++ sql-crud.py
@@ -94,12 +94,48 @@ lorraine_scanlon = Programmer(
 #session.add(margaret_hamilton)
 #session.add(bill_gates)
 #session.add(tim_berners_lee)
-session.add(lorraine_scanlon)
+#session.add(lorraine_scanlon)
 
 #commit our session to the database
-session.commit()
+#session.commit()
 
-#query the database to find all programmers
+# updating a single record
+#programmer = session.query(Programmer).filter_by(id = 8).first()
+#programmer.famous_for = "World President"
+
+# updating multiple records
+#people = session.query(Programmer)
+#for person in people:
+#    if person.gender == "F":
+#        person.gender = "Female"
+#    elif person.gender == "M":
+#        person.gender = "Male"
+#    else:
+#        print("Gender not defined")
+#    session.commit()
+
+# deleting a single record
+#fname = input("Enter a first name: ")
+#lname = input("Enter a last name: ")
+#programmer = session.query(Programmer).filter_by(first_name=fname, last_name=lname).first()
+# defensive programming
+#if programmer is not None:
+#    print("Programmer Found: ", programmer.first_name +" " + programmer.last_name)
+#    confirmation = input("Are you sure you want to delete this record? (y/n) ")
+#    if confirmation.lower() == "y":
+#        session.delete(programmer)
+#        session.commit()
+#        print("Programmer has been deleted")
+#    else:
+#        print("Programmer not deleted")
+#else:
+#    print("No records found")
+
+
+#commit our session to the database
+#session.commit()
+
+# query the database to find all programmers
 programmers = session.query(Programmer)
 for programmer in programmers:
     print(
